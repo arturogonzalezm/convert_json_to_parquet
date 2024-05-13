4d8f0fdf9344bd9d6988777578a03c6bf7003bdb
Arturo Gonzalez <arturogonzalez@192-168-1-107.tpgi.com.au>
2024-05-13 14:07:15 +1000

    Improved action

--- src/utilities/custom_exceptions.py
+++ src/utilities/custom_exceptions.py
@@ -1,6 +1,25 @@
+"""
+This module contains custom exceptions that are raised during the ETL job.
+"""
+
+
 class ETLJobError(Exception):
+    """
+    Exception raised for errors that occur during the ETL job.
+
+    Attributes:
+    - message (str): Explanation of the error.
+    """
+
     pass
 
 
 class SparkSessionError(Exception):
+    """
+    Exception raised for errors related to the Spark session.
+
+    Attributes:
+    - message (str): Explanation of the error.
+    """
+
     pass
